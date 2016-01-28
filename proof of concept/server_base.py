1e58e371c8f6a06cba2f0581281b70f7af7d8a0a
josh <coyle5280@gmail.com>
2016-01-27 23:22:06 -0700

    plugin acts like a telnet session....asks for login and password and prints those items

--- proof of concept/server_base.py
+++ proof of concept/server_base.py
@@ -1,17 +1,28 @@
 import os
 import sys
+import socket
 
 path = "plugins/"
 plugins = {}
+HOST = ''
+ports = []
 
 sys.path.insert(0, path)
 for i in os.listdir(path):
     fname, ext = os.path.splitext(i)
     if ext == '.py':
         mod = __import__(fname)
-        plugins[fname] = mod.Plugin()
+        if mod.Plugin():
+            plugins[fname] = mod.Plugin()
+            print "Plugin loaded: " + fname
+            plugin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            port = plugins[fname].get_port()
+            print "Opening Socket for " + fname + " on Port: " + str(port)
+            plugin_socket.bind((HOST, int(port)))
+            ports.append(port)
+            plugins[fname].run(plugin_socket)
 sys.path.pop(0)
 
-for plugin in plugins.values():
-    plugin.run()
+#for plugin in plugins.values():
+ #   plugin.run()
 
