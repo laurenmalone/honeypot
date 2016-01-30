012afc9248f66537355454897dd363544fe2638d
root <root@debian.home>
2016-01-30 12:48:31 -0700

    Added files for geoip decoder and geojson.  See file geoip_poc.py for command line args to install additional python packages

---
+++ proof of concept/geoIp_poc.py
@@ -0,0 +1,42 @@
+# You will need to install python packages python-geoip, python-pip, and python-wheel
+# Debian "jessie" commands:
+# apt-get install python-geoip
+# apt-get install python-pip python-wheel
+# pip install geojson
+
+import GeoIP
+import geojson
+
+giDB = GeoIP.open("geoIP/GeoLiteCity.dat", GeoIP.GEOIP_INDEX_CACHE | GeoIP.GEOIP_CHECK_CACHE)
+
+
+def convert_to_geojson_point(ip_record):
+    return geojson.Point((ip_record["latitude"], ip_record["longitude"]))
+
+
+def convert_to_geojson_feature(ip_record):
+    # print ip_record
+    convert_to_geojson_point(ip_record)
+    feature = geojson.Feature(geometry=convert_to_geojson_point(ip_record))
+    feature["properties"] = {
+        "city": ip_record["city"],
+        "region_name": ip_record["region_name"],
+        "region": ip_record["region"],
+        "area_code": ip_record["area_code"],
+        "time_zone": ip_record["time_zone"],
+        "metro_code": ip_record["metro_code"],
+        "country_code3": ip_record["country_code3"],
+        "postal_code": ip_record["postal_code"],
+        "dma_code": ip_record["dma_code"],
+        "country_code": ip_record["country_code"],
+        "country_name": ip_record["country_name"]
+    }
+    return feature
+
+
+def get_record_from_geoip(ip_address):
+    return giDB.record_by_name(ip_address)
+
+record = get_record_from_geoip("5.149.176.5")
+print convert_to_geojson_point(record)
+print convert_to_geojson_feature(record)
--- proof of concept/server_base.py
+++ proof of concept/server_base.py
@@ -21,12 +21,8 @@ plugins = {}
 HOST = ''
 ports = []
 
-sys.path.insert(0, path)
-
 
 def setup_db_table(orm):
-    #print orm
-    #print "Creating Table: " + orm['table']['tableName']
     logging.info("Creating Table: " + orm['table']['tableName'] + ":Time: " + str(my_date_time.now()))
     statement = 'CREATE TABLE ' + orm['table']['tableName'] + '(ID INT PRIMARY KEY NOT NULL,'
     for item in orm['table']:
@@ -36,28 +32,36 @@ def setup_db_table(orm):
     statement += ');'
     conn.execute(statement)
 
-for i in os.listdir(path):
-    fname, ext = os.path.splitext(i)
-    if ext == '.py':
-        print "Loading File: " + fname
-        logging.info("Loading File: " + fname + ":Time: " + str(my_date_time.now()))
-        mod = __import__(fname)
-        if mod.Plugin():
-            plugins[fname] = mod.Plugin()
 
-            print "Plugin loaded: " + fname
-            logging.info("Plugin loaded: " + fname + ":Time: " + str(my_date_time.now()))
+def load_plugins():
+    for i in os.listdir(path):
+        fname, ext = os.path.splitext(i)
+        if ext == '.py':
+            print "Loading File: " + fname
+            logging.info("Loading File: " + fname + ":Time: " + str(my_date_time.now()))
+            mod = __import__(fname)
+            if mod.Plugin():
+                plugins[fname] = mod.Plugin()
 
-            plugin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            port = plugins[fname].get_port()
-            setup_db_table(plugins[fname].get_orm())
-            print "Opening Socket for " + fname + " on Port: " + str(port)
-            logging.info("Plugin loaded: " + fname + ":Time: " + str(my_date_time.now()))
+                print "Plugin loaded: " + fname
+                logging.info("Plugin loaded: " + fname + ":Time: " + str(my_date_time.now()))
 
-            plugin_socket.bind((HOST, int(port)))
-            ports.append(port)
-            plugins[fname].run(plugin_socket)
+                plugin_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                port = plugins[fname].get_port()
+                setup_db_table(plugins[fname].get_orm())
+                print "Opening Socket for " + fname + " on Port: " + str(port)
+                logging.info("Plugin loaded: " + fname + ":Time: " + str(my_date_time.now()))
+
+                plugin_socket.bind((HOST, int(port)))
+                ports.append(port)
+                plugins[fname].run(plugin_socket)
+
+
+sys.path.insert(0, path)
+load_plugins()
 sys.path.pop(0)
+
+
 
 #for plugin in plugins.values():
  #   plugin.run()
