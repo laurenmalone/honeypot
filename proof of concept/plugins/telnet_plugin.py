26de5b3aaa129413bdb6d1abb5f00228e47fc9e0
josh <coyle5280@gmail.com>
2016-01-31 22:43:50 -0700

    Minor changes to the telnet plugin

--- proof of concept/plugins/telnet_plugin.py
+++ proof of concept/plugins/telnet_plugin.py
@@ -1,9 +1,5 @@
 class Plugin:
 
-    import socket
-    import sys
-
-    PORT = 8888
     ORM = {"table": {
         'tableName': "test_table2",
         "column": {
@@ -14,9 +10,15 @@ class Plugin:
 
     def __init__(self):
         print "Module Loaded and waiting on run() command"
+        self.sqliteDbConn = None
+        self.geoIp = None
+        self.passwords = []
+        self.count =0
+        self.login = ""
+        self.PORT = 8888
 
     def run(self, passed_socket):
-        print("Port Number: " + passed_socket.getsockname()[0])
+        #print("Port Number: " + passed_socket.getsockname()[0])
         if passed_socket:
             passed_socket.listen(10)
             while 1:
@@ -26,11 +28,16 @@ class Plugin:
                 print 'socket type: ' + str(passed_socket.type)
                 print 'socket proto: ' + str(passed_socket.proto)
                 conn.sendall("Login: ")
-                login = conn.recv(64)
-                print 'Login : ' + login
-                conn.sendall("Password: ")
-                password = conn.recv(64)
-                print 'Password : ' + password
+                self.login = conn.recv(64)
+                print 'Login : ' + self.login
+                while self.count < 3:
+                    conn.sendall("Password: ")
+                    password = conn.recv(64)
+                    self.passwords.append(password)
+                    print 'Password : ' + password
+                    conn.sendall("---Incorrect--\n")
+                    conn.sendall("Password: ")
+                    self.count = + 1
             passed_socket.close()
         else:
             print "socket error"
@@ -40,3 +47,9 @@ class Plugin:
 
     def get_orm(self):
         return self.ORM
+
+    def set_db_conn(self, conn):
+        self.sqliteDbConn = conn
+
+    def set_geoip(self, geoip):
+        self.geoIp = geoip
