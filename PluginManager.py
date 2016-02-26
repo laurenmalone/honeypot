8450b7b15e5cc40d1f34e7d7acf0266d4fc33e42
Griffin DeJohn <gdejohn@gmail.com>
2016-02-26 12:49:04 -0700

    Stub out PluginManager

---
+++ PluginManager.py
@@ -0,0 +1,11 @@
+class PluginManager:
+    def __init__(self, plugin, session_factory):
+        self._plugin = plugin
+        self._session_factory = session_factory
+
+    def run(self):
+        pass
+
+    def stop(self):
+        pass
+
