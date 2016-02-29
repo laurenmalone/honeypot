eb54cb8758deb77a30d1a64b6b4dfef7e1014a01
Lauren Malone <lmalone4@msudenver.edu>
2016-02-29 12:48:26 -0700

    Change honeypot base methods to private using underscore convention

--- HoneypotBase.py
+++ HoneypotBase.py
@@ -2,23 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from PluginManager import PluginManager
 import os
-import datetime
 import sys
-
-my_date_time = datetime.datetime
-plugins = {}
 
 
 class HoneypotBase:
     def __init__(self):
-        self._plugin_directory = "./proof_of_concept/plugins/"
+        self._plugin_directory = './proof_of_concept/plugins/'
         self._threads = []
-        self.engine = create_engine('sqlite:///sqliteDB/test.db', echo=True)
-        self.Session = sessionmaker(bind=self.engine)
+        self._engine = create_engine('sqlite:///sqliteDB/test.db', echo=True)
+        self._Session = sessionmaker(bind=self._engine)
 
-    # call start_manager_thread() for each item in plugins
-    def load_plugins(self):
-
+    def _load_plugins(self):
         sys.path.insert(0, self._plugin_directory)
         for i in os.listdir(self._plugin_directory):
             filename, ext = os.path.splitext(i)
@@ -26,50 +20,54 @@ class HoneypotBase:
                 print "Loading File: " + filename
                 mod = __import__(filename)
                 if mod.Plugin():
-                    plugins[filename] = mod.Plugin()
-                print "Plugin loaded: " + filename
-                HoneypotBase.start_manager_thread(self,mod.Plugin())
+                    print "Plugin loaded: " + filename
+                    HoneypotBase._start_manager_thread(self, mod.Plugin())
+        sys.path.pop(0)
 
-    def start_manager_thread(self, plugin):
-        thread = PluginManager(plugin, self.Session)
+    def _start_manager_thread(self, plugin):
+        thread = PluginManager(plugin, self._Session)
         thread.start()
         self._threads.append(thread)
 
     # listens for user commands
     @staticmethod
     def start_interface():
+        print("waiting for command")
         while True:
-            print("waiting for command")
             command = raw_input()
             if command == "start honeypot":
-                HoneypotBase().load_plugins()
+                HoneypotBase()._load_plugins()
             elif command == "stop honeypot":
-                HoneypotBase().stop_hp()
+                HoneypotBase()._stop_hp()
             elif command == "start server":
-                HoneypotBase().start_server()
+                HoneypotBase()._start_server()
             elif command == "stop server":
-                HoneypotBase().stop_server()
+                HoneypotBase()._stop_server()
             elif command == "start all":
-                sys.path.insert(0, HoneypotBase()._plugin_directory)
-                HoneypotBase().load_plugins()
-                HoneypotBase().start_server()
+                HoneypotBase()._load_plugins()
+                HoneypotBase()._start_server()
+            elif command == "stop all":
+                HoneypotBase()._stop_server()
+                HoneypotBase()._stop_hp()
             elif command == "help":
-                pass
+                print ("start honeypot")
+                print ("stop honeypot")
+                print ("start server")
+                print ("stop server")
+                print ("start all")
+                print ("stop all")
             else:
-                print("command not found\n")
+                print("command not found")
 
-    # kill program - kill manager threads
-    def stop_hp(self):
+    def _stop_hp(self):
         for thread in self._threads:
             thread.stop()
         raise SystemExit(0)
 
-    def start_server(self):
+    def _start_server(self):
         pass
 
-    def stop_server(self):
-        sys.path.pop(0)
+    def _stop_server(self):
         pass
-
 
 HoneypotBase().start_interface()
