db600d33f3df9f4b2a8080a3dfc04bb558dd6d03
Griffin DeJohn <gdejohn@gmail.com>
2016-03-04 12:18:05 -0700

    Use threading.Event in PluginManager

--- PluginManager.py
+++ PluginManager.py
@@ -1,12 +1,12 @@
 import socket
-from threading import Thread
+from threading import Thread, Event
 
 class PluginManager(Thread):
     def __init__(self, plugin, session_factory):
         Thread.__init__(self)
         self._plugin = plugin
         self._session_factory = session_factory
-        self._flag = 0
+        self._flag = Event()
 
     def run(self):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,7 +15,7 @@ class PluginManager(Thread):
         serversocket.listen(5)
         while 1:
             clientsocket, address = serversocket.accept()
-            if self._flag:
+            if self._flag.is_set():
                 clientsocket.close()
                 break
             else:
@@ -24,7 +24,7 @@ class PluginManager(Thread):
         serversocket.close()
 
     def stop(self):
-        self._flag = 1
+        self._flag.set()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(('localhost', self._plugin.get_port()))
         sock.close()
