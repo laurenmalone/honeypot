c8e407b21dd4cca567bde21d624083a2651c5fad
Christi Kazakov <ckazakov18@gmail.com>
2016-03-14 13:51:40 -0600

    cleanup telnet_plugin

--- plugins/telnet_plugin.py
+++ plugins/telnet_plugin.py
@@ -1,24 +1,25 @@
-import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from geoip import geolite2
 
 Base = declarative_base()
+
+
+class Telnet(Base):
+    __tablename__ = 'telnet'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    geo_ip = Column(String)
 
 
 class Plugin:
 
     ORM = {"table": {"table_name": "telnet",
-                     "column":[{"name": "username", "type": "TEXT"},
-                               {"name": "password", "type": "TEXT"},
-                               {"name": "ip", "type": "TEXT"}]
+                     "column": [{"name": "username", "type": "TEXT"},
+                                {"name": "password", "type": "TEXT"},
+                                {"name": "ip", "type": "TEXT"}]
                      }}
-
-    class Telnet(Base):
-        __tablename__ = 'telnet'
-        id = Column(Integer, primary_key=True)
-        username = Column(String)
-        password = Column(String)
-        geo_ip = Column(String)
 
     def __init__(self):
         # print "Module Loaded and waiting on run() command"
@@ -28,49 +29,49 @@ class Plugin:
         self.username = []
         self.PORT = 8888
 
-    def run(self,passed_socket, address, session):
-         # print("Port Number: " + passed_socket.getsockname()[0])
-        if passed_socket:
-            # need to sleep thread if no answer
+    def run(self, socket, address, session):
+        # print("Port Number: " + socket.getsockname()[0])
+        socket.settimeout(35)
+        if socket:
             # for loop and try catch the timeout exception
-            # while self.count < 3:
-            # may have to save the timeout for the next iteration
-            for i in range(0,3):
+            for i in range(0, 3):
                 # try catch
-                passed_socket.settimeout(35)
-                passed_socket.sendall("username:")
-                username = passed_socket.recv(64)
-                self.username.append(username)
-                print 'Username : ' + username
-                passed_socket.sendall("Password: ")
-                password = passed_socket.recv(64)
-                self.passwords.append(password)
-
-                # if they take too long to enter password
+                socket.sendall("username:")
+                try:
+                    # this sets the timeout and times out after 35 seconds
+                    username = socket.recv(64)
+                except socket.timeout:
+                    print 'timeout error'
+                else:
+                    # otherwise it receives the data and shuts the timeout off
+                    username = socket.recv(64)
+                    socket.settimeout(None)
+                    self.username.append(username)
+                print 'username' + username
+                socket.sendall("Password: ")
+                try:
+                    password = socket.recv(64)
+                except socket.timeout:
+                    print 'timeout error'
+                else:
+                    password = socket.recv(64)
+                    # socket.settimeout(None)
+                    self.passwords.append(password)
                 # session.add_all([User(username='', password=''),
-                #                   User(username='', password='')])
+                # User(username='', password='')])
                 # session.commit()
                 print 'Password : ' + password
-                passed_socket.sendall("---Incorrect--\n")
-                #passed_socket.sendall("Password: ")
+                socket.sendall("---Incorrect--\n")
+                # socket.sendall("Password: ")
                 self.count += 1
-                #record = Telnet(username= '<name>', password = '<password>')
-                #session.add(record)
-                #session.commit()
-                #session.close()
-            passed_socket.close()
-            self.save_session_and_exit(session)
-            sys.exit(0)
+                record = Telnet(username=username, password=password, geo_ip=self.get_geo_ip(address[0]))
+                session.add(record)
+                session.commit()
+                session.close()
+
+            socket.close()
         else:
             print "socket error"
-
-    def save_session_and_exit(self, db_session):
-        for x in range(len(self.username)):
-            print ""
-            #record = self.Telnet('<name>' = self.username[x], password='<password>')
-            #b_session.add(record)
-            #db_session.commit()
-        db_session.close()
 
     def get_port(self):
         return self.PORT
@@ -78,5 +79,7 @@ class Plugin:
     def get_orm(self):
         return self.ORM
 
-    def get_geo_ip(self, passed_socket):
+    def get_geo_ip(self, host):
+        match = geolite2.lookup(host)
+        self.geo_ip = match.location  # (latitude, longitude)
         return self.geo_ip
