952a75fb4d3ec65faea76886429037e4e56d2f15
Lauren Malone <lmalone4@msudenver.edu>
2016-04-05 22:01:59 -0600

    Refactor HoneypotBase
    To account for catchall feature

--- HoneypotBase.py
+++ HoneypotBase.py
@@ -7,7 +7,6 @@ import sys
 import logging
 import datetime
 import signal
-import traceback
 from base import Base
 from ConfigParser import SafeConfigParser
 import ast
@@ -44,6 +43,7 @@ def _read_config():
 
     Get locations of db, log, and plugins.
     Create engine and sessionmaker for interacting with db.
+    Get list of ports to listen on.
     """
     global _db, _plugin_directory, log, _engine, _Session, _catch_all_list
     parser = SafeConfigParser()
@@ -58,6 +58,7 @@ def _read_config():
 
     logging.basicConfig(filename=log, level=logging.DEBUG)
     _catch_all_list = ast.literal_eval(parser.get('honeypot', 'ports'))
+    print _catch_all_list
 
 
 def _import_plugins():
@@ -75,104 +76,71 @@ def _import_plugins():
 
             if filename == '__init__' or ext != '.py' or filename == 'dummy_plugin':
                 continue
-            print filename
+
             try:
                 mod = __import__(filename)
                 plugin = mod.Plugin()
-                display = plugin.get_display()
                 port = plugin.get_port()
-
-                if not _port_valid(port):
-                    logging.exception(filename + " not loaded :Time: " + str(my_date_time.now()))
-
-                elif port in _catch_all_list:
+                if port in _catch_all_list:
                     _catch_all_list.remove(port)
-                    print _catch_all_list
                     add_item_to_table(plugin)
+                    add_item_to_plugin_list(plugin)
 
-                    if plugin.get_port == 0:  # add to end of list
-                        _plugin_list.append(plugin)
-
-                    else:
-                        _plugin_list.insert(0, plugin)  # add to beginning of list
-
-                    print (filename + ext + " successfully loaded")
             except:
-                logging.exception(filename + " not loaded " ":Time: " + str(my_date_time.now()))
+                logging.exception(filename + " cannot be loaded " ":Time: " + str(my_date_time.now()))
 
         sys.path.pop(0)
+
+
+def add_item_to_plugin_list(plugin):
+    port = plugin.get_port()
+    if _port_valid(port):
+        if port == 0:  # add to end of list
+            _plugin_list.append(plugin)
+
+        else:
+            _plugin_list.insert(0, plugin)  # add to beginning of list
+        print plugin.get_display() + " successfully loaded " + str(plugin.get_port())
+        return True
+
+    else:
+        logging.exception(plugin.get_value() + " not loaded, port already in use :Time: " + str(my_date_time.now()))
+        return False
+
+
+def _start_catchall_plugins():
+    print _catch_all_list
+    if len(_catch_all_list) < 0:
+        return False
+
+    import plugins.dummy_plugin
+    add_item_to_table(plugins.dummy_plugin.Plugin())
+    for i in range(len(_catch_all_list)):
+        dummy = plugins.dummy_plugin.Plugin()
+        port = _catch_all_list[i]
+        dummy.set_port(port)
+        add_item_to_plugin_list(dummy)
+        return True
 
 
 def add_item_to_table(plugin):
-
     try:
         session = _Session()
-        print 'here'
         q = session.query(Plugin.id).filter(Plugin.display == plugin.get_display())
         if q.count() > 0:
             return
+
         record = Plugin(display=plugin.get_display(), description=plugin.get_description(),
                         orm=str(plugin.get_orm()), value=(plugin.get_value()))
-
         session.add(record)
         session.commit()
         session.close()
-        print plugin.get_display + " added"
+        #print plugin.get_display() + " added to plugin table"
+        return True
+
     except:
         logging.exception(plugin.get_value + " could not be added to plugins table :Time: " + str(my_date_time.now()))
-
-
-
-    """
-    def _import_plugins():
-    Import plugins.
-
-    Add successfully imported plugins to plugin_list.
-    return: bool -- False if plugin folder cannot be found, True otherwise.
-    Raise OSError if plugin folder isn't found.
-    Raise exception if a plugin cannot be imported.
-
-    try:
-        sys.path.insert(0, _plugin_directory)
-        os.listdir(_plugin_directory)
-
-    except OSError:
-        print("Plugin folder not found.")
         return False
-
-    else:
-        for i in os.listdir(_plugin_directory):
-            filename, ext = os.path.splitext(i)
-            if filename == '__init__' or ext != '.py':
-                continue
-            #print filename + ext+ " loading..."
-
-            try:
-                mod = __import__(filename)
-                plugin = mod.Plugin()
-                if not _port_valid(plugin.get_port()):
-                    logging.exception(filename + " not loaded :Time: " + str(my_date_time.now()))
-                    print (filename + " not loaded. Port " + str(plugin.get_port()) + " not available.")
-
-                else:
-                    if plugin.get_port == 0:  # add to end of list
-                        _plugin_list.append(plugin)
-
-                    else:
-                        _plugin_list.insert(0, plugin)  # add to beginning of list
-
-                    print (filename + ext + " successfully loaded")
-            except AttributeError:
-                logging.exception(filename + " not loaded " ":Time: " + str(my_date_time.now()))
-            except Exception as e:
-                traceback.print_exc(file=sys.stdout)
-                print(e)
-                logging.exception(filename + " not loaded " ":Time: " + str(my_date_time.now()))
-                print (filename + ext + " not loaded")
-
-        sys.path.pop(0)
-        return True"""
-
 
 def _port_valid(port):
     """Check that given port is valid and available.
@@ -198,46 +166,11 @@ def _create_plugin_tables():
     """
     try:
         Base.metadata.create_all(_engine)
-    except InvalidRequestError as e:
-        print(e)
-    except Exception as e:
-        #print(e)
+    except:
         logging.exception("plugin table not created " ":Time: " + str(my_date_time.now()))
 
 
-"""def _add_items_to_plugin_table():
-    Insert an item into plugin table for each item in plugins_list.
 
-    Raise AttributeError if a plugin does not have attributes required for visual tool.
-    Raise SQLAlchemyError if an item is not added to plugin table successfully.
-
-    session = _Session()
-    for i in _plugin_list:
-
-        try:
-            q = session.query(Plugin.id).filter(Plugin.display == i.get_display())
-            if q.count() > 0:
-                continue
-            record = Plugin(display=i.get_display(), description=i.get_description(),
-                            orm=str(i.get_orm()), value=(i.get_value()))
-
-        except AttributeError:
-            print "Plugin does not have attributes to use visual tool"
-            logging.exception("Plugin does not have attributes to use visual tool :Time: " + str(my_date_time.now()))
-        except Exception:
-            print "error"
-        else:
-            try:
-                session.add(record)
-                session.commit()
-            except SQLAlchemyError as e:
-                print(e)
-                logging.exception("record not added to table " ":Time: " + str(my_date_time.now()))
-            except Exception:
-                print "error"
-    session.close()
-
-    """
 def _start_manager_threads():
     """Start a PluginManager thread for each item in plugins
     """
@@ -257,21 +190,15 @@ def _signal_handler(signal, frame):
 def run():
     """ Drive program."""
     _read_config()
-
-
-
     _create_plugin_tables()
-
     _import_plugins()
+    _start_catchall_plugins()
+    _start_manager_threads()
 
-    #_add_items_to_plugin_table()
-
-   # _start_manager_threads()
-
-    # wait for program to be killed
-    #signal.signal(signal.SIGINT, _signal_handler)
-    #signal.signal(signal.SIGTERM, _signal_handler)
-    #signal.pause()
+    #wait for program to be killed
+    signal.signal(signal.SIGINT, _signal_handler)
+    signal.signal(signal.SIGTERM, _signal_handler)
+    signal.pause()
 
 
 if __name__ == "__main__":
